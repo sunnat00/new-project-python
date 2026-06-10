afc032a51e28f247722bcc5c9c6c4cccdf770479
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-06-11 00:24:46 +0200

    yangi loyha

---
+++ project_07.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import messagebox
+
+root=Tk()
+root.geometry("600x700+400+80")
+root.resizable(False,False)
+root.title("Voice Recorder")
+
+
+
+
+
+
+
+
+
+root.mainloop()
